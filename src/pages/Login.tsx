90439c2ba40b205c4aca0edc8932498aa818e077
bapibiswas100808 <star.bappy@gmail.com>
2025-01-20 00:50:52 +0600

    authemtication going

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,43 @@
+import { useForm } from "react-hook-form";
+import { useLoginMutation } from "../redux/features/auth/authApi";
+import { useAppDispatch } from "../redux/hooks";
+import { setUser } from "../redux/features/auth/authSlice";
+import { verifyToken } from "../utils/verifyToken";
+
 const Login = () => {
-  return <div>login</div>;
+  const dispatch = useAppDispatch();
+  const { register, handleSubmit } = useForm({
+    defaultValues: {
+      userId: "0001",
+      password: "admin12345",
+    },
+  });
+  const [login, { data, error }] = useLoginMutation();
+  console.log("data", data);
+  console.log("error", error);
+
+  const onSubmit = async (data) => {
+    const userInfo = {
+      id: data.userId,
+      password: data.password,
+    };
+    const res = await login(userInfo).unwrap();
+    const user = verifyToken(res.data.accessToken);
+    dispatch(setUser({ user: user, token: res.data.accessToken }));
+  };
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div>
+        <label htmlFor="id">ID:</label>
+        <input type="text" id="id" {...register("userId")} />
+      </div>
+      <div>
+        <label htmlFor="password">Password:</label>
+        <input type="text" id="password" {...register("password")} />
+      </div>
+      <button type="submit">Login</button>
+    </form>
+  );
 };
 
 export default Login;
